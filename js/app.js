db3664f5982f04baaba517d9a367cb1611676185
cesray <cesar.rayas.c@gmail.com>
2021-06-30 20:46:23 -0500

    - created welcome function in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log("Hola Programador, eres genial :)")
+console.log('Use welcome function ;)')
+
+function welcome() {
+  for (let i = 0; i < 10; i++) {
+    console.log('thanks for being here!')
+  }
+}
